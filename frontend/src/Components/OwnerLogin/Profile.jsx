ca9ec53eba7bdba3f5ddfea829d507b4823b4267
Praveen-Mariappasamy <115199875+Praveen-Mariappasamy@users.noreply.github.com>
2024-05-31 17:08:28 +0530

    Update Profile.jsx

--- frontend/src/Components/OwnerLogin/Profile.jsx
+++ frontend/src/Components/OwnerLogin/Profile.jsx
@@ -13,7 +13,7 @@ const Profile = () => {
         const email=localStorage.getItem('email')
         const fetchData = async () => {
             try {
-                const response = await fetch('https://rentify-eosin-theta.vercel.app/protected', {
+                const response = await fetch('https://rentify-app-api.vercel.app/protected', {
                     method:'POST',
                     headers:{
                         Accept:'application/json',
