b0bead066385d951f8c377dcc543a611ec16f058
Praveen-Mariappasamy <115199875+Praveen-Mariappasamy@users.noreply.github.com>
2024-05-31 16:28:12 +0530

    Update multerConfig.js

--- backend/multerConfig.js
+++ backend/multerConfig.js
@@ -2,12 +2,12 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: './upload/images',
-    filename:(req,file,cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    destination: '/tmp', 
+    filename:(req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
 });
 
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 module.exports = upload;
