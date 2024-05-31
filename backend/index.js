c7ebcdaf0f8b4dcaf2bb7225fd6031b1fe1ce2da
Praveen-Mariappasamy <115199875+Praveen-Mariappasamy@users.noreply.github.com>
2024-05-31 16:59:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors(
     {
-        origin: ["https://deploy-mern-1whq.vercel.app"],
+        origin: ["https://rentify-kjn9.vercel.app/"],
         methods: ["POST","GET"],
         credentials: true
     }
